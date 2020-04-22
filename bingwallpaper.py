a5339b6eaf499ae2fbde52c738810a5ba1f22e1f
chengyijun <cyjmmy@qq.com>
2020-04-22 20:41:02 +0800

    简单修复

--- bingwallpaper.py
+++ bingwallpaper.py
@@ -33,7 +33,7 @@ class BingWallPaper():
         'User-Agent':
             'Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.112 Safari/537.36'
     }
-    IMGS_ROOT = os.path.join(os.path.dirname(__file__),
+    IMGS_ROOT = os.path.join(os.getcwd(),
                              'images').replace('\\', '/')
 
     def __init__(self):
